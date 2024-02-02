24add5fb6bc7fd885943c7953a0a1bf0f5a78714
Alexander Strutz <alex-strutz@t-online.de>
2024-02-02 16:52:40 +0100

    Implement creation form for lat lng

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { TabViewModule } from 'primeng/tabview';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
   imports: [
     CardModule,
+    InputTextModule,
+    InputGroupModule,
+    TabViewModule,
+    TabMenuModule,
+    InputNumberModule,
+    ButtonModule,
   ],
   templateUrl: './create.component.html',
 })
 export class CreateComponent {
-
+  constructor(private _router: Router) {
+  }
+  protected navigateBack() {
+    this._router.navigate(['']);
+  }
 }
