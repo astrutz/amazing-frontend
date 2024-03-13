3082c95592fab6076536441940a9083a2ee07ab3
Alexander Strutz <alex-strutz@t-online.de>
2024-03-13 14:35:28 +0100

    Try cyclic

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,14 +1,12 @@
 import { Injectable, isDevMode } from '@angular/core';
 import axios from 'axios';
 import { Marker } from '../types/marker.type';
-// @ts-ignore
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
-  private _url = isDevMode() ? '/api/markers' : 'http://2.58.113.80:3000/markers';
+  private _url = isDevMode() ? '/api/markers' : 'https://concerned-fish-gabardine.cyclic.app/';
 
   async getMarkers(): Promise<Marker[]> {
     return (await axios.get<Marker[]>(this._url)).data;
