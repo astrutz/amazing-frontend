b24f1cc9419caec26dc8b0b64d1c799296807633
Alexander Strutz <alex-strutz@t-online.de>
2024-03-13 14:11:59 +0100

    Change post requests different on prod

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import axios from 'axios';
 import { Marker } from '../types/marker.type';
 
@@ -6,11 +6,13 @@ import { Marker } from '../types/marker.type';
   providedIn: 'root'
 })
 export class RequestService {
+  private _url = isDevMode() ? '/api/markers' : 'http://2.58.113.80:3000/markers';
+
   async getMarkers(): Promise<Marker[]> {
-    return (await axios.get<Marker[]>('/api/markers')).data;
+    return (await axios.get<Marker[]>(this._url)).data;
   }
 
   async createMarker(markerData: Marker): Promise<void> {
-    await axios.post('/api/markers', markerData);
+    await axios.post(this._url, markerData);
   }
 }
