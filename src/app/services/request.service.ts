ec6a6dc5a2c422f4fecf51345cc08772905f2a5c
Alexander Strutz <alex-strutz@t-online.de>
2024-03-13 14:32:02 +0100

    Re-add http

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,12 +1,14 @@
 import { Injectable, isDevMode } from '@angular/core';
 import axios from 'axios';
 import { Marker } from '../types/marker.type';
+// @ts-ignore
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
-  private _url = isDevMode() ? '/api/markers' : '2.58.113.80:3000/markers';
+  private _url = isDevMode() ? '/api/markers' : 'http://2.58.113.80:3000/markers';
 
   async getMarkers(): Promise<Marker[]> {
     return (await axios.get<Marker[]>(this._url)).data;
