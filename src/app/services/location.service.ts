354a152f314f98badb8fdf89ae1fdfc91daabaf4
m.uppenkamp <m.uppenkamp@babiel.com>
2024-04-10 16:38:29 +0200

    Fix warnings

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -11,7 +11,7 @@ type SetLocationValue = {
 }
 
 /**
- * This service is intended to provide the whole app with a somewhat useful and reliable current location. 
+ * This service is intended to provide the whole app with a somewhat useful and reliable current location.
  */
 @Injectable({ providedIn: 'root' })
 class CurrentLocationService {
@@ -19,12 +19,12 @@ class CurrentLocationService {
   private readonly _positioningOpts: PositionOptions = {
     enableHighAccuracy: true,
     /** Allows to work with the cached location for one minute. One sticker at a time! */
-    maximumAge: 1 * 60 * 1000,
-    /** Noone wants to wait more than 5sec, right? */
+    maximumAge: 60 * 1000,
+    /** No one wants to wait more than 5sec, right? */
     timeout: 5 * 1000
   }
   /**
-   * Somewhere one has to be. At least in one’s heart. Let us give those a home who don’t have one (yet). 
+   * Somewhere one has to be. At least in one’s heart. Let us give those a home who don’t have one (yet).
     */
   private _homeBase: GeolocationCoordinates = {
     latitude: 51.21957,
@@ -64,7 +64,7 @@ class CurrentLocationService {
 
   /**
    * Force updates the current position provided by this service
-   * @returns {Promise<void>} Rejects on any error; its up to the user wheter to work with the - then - stale position or ignore this service’s position.
+   * @returns {Promise<void>} Rejects on any error; it's up to the user whether to work with the - then - stale position or ignore this service’s position.
    */
   public async update(): Promise<void> {
     return new Promise((resolve, reject) => {
