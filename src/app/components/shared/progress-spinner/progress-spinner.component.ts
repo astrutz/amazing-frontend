75df504b4b915724c76b3a937568757450070b7e
m.uppenkamp <m.uppenkamp@babiel.com>
2024-04-10 18:30:26 +0200

    Add progress spinner

---
+++ src/app/components/shared/progress-spinner/progress-spinner.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'progress-spinner',
+  templateUrl: 'progress-spinner.component.html',
+  styleUrl: 'progress-spinner.component.scss'
+})
+export class ProgressSpinnerComponent {
+  @Input()
+  show: boolean = false;
+}
