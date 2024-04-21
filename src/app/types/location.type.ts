2e39caddf4e9d82e1801e1d529af25434fb1b14d
Philipp Müller <1437939-muellerphilipp@users.noreply.gitlab.com>
2024-04-21 12:23:47 +0200

    Moved type to types; fixed typos

---
+++ src/app/types/location.type.ts
@@ -0,0 +1,11 @@
+type SetLocationValue = {
+  latitude: number,
+  longitude: number,
+  accuracy?: number,
+  altitude?: number,
+  altitudeAccuracy?: number,
+  heading?: number,
+  speed?: number
+}
+
+export { SetLocationValue }
