183e17854797fe0cc162b04b199bc72479ac802f
Philipp Müller <1437939-muellerphilipp@users.noreply.gitlab.com>
2024-04-22 18:34:30 +0200

    Change component for create route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { CreateComponent } from './components/create/create.component';
 
 export const routes: Routes = [
   {
     path: 'create',
-    component: CreateComponent,
+    component: HomeComponent,
   },
   {
     path: '',
