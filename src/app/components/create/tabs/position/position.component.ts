a9d6a9d5af8b53f35e2a2d24c19083972f0ddd4c
m.uppenkamp <35469083+MUppenkamp@users.noreply.github.com>
2025-10-27 21:38:51 +0100

    Add loading spinner

--- src/app/components/create/tabs/position/position.component.ts
+++ src/app/components/create/tabs/position/position.component.ts
@@ -1,20 +1,18 @@
-import {ChangeDetectionStrategy, Component, computed, effect, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
 import {LocationService} from "../../../../services/location.service";
+import {ProgressSpinnerComponent} from "../../../shared/progress-spinner/progress-spinner.component";
 
 @Component({
   selector: 'app-position',
   templateUrl: './position.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ProgressSpinnerComponent
+  ],
   standalone: true
 })
 export class PositionComponent {
   protected readonly locationService = inject(LocationService);
-
-  constructor() {
-    effect(() => {
-      console.log('currentLocation$().coords', this.currentLocation$().coords)
-    });
-  }
 
   /**
    * Current user positon
