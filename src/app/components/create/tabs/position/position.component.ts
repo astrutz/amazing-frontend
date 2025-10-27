17d54fe5c00acf27cd931e88b76bbd61dd4e0ecd
m.uppenkamp <35469083+MUppenkamp@users.noreply.github.com>
2025-10-27 22:45:45 +0100

    Add retry geolocation

--- src/app/components/create/tabs/position/position.component.ts
+++ src/app/components/create/tabs/position/position.component.ts
@@ -1,15 +1,22 @@
-import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
-import {LocationService} from "../../../../services/location.service";
-import {ProgressSpinnerComponent} from "../../../shared/progress-spinner/progress-spinner.component";
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { LocationService } from "../../../../services/location.service";
+import { ProgressSpinnerComponent } from "../../../shared/progress-spinner/progress-spinner.component";
+import { NgIcon, provideIcons } from "@ng-icons/core";
+import { ionRefresh } from "@ng-icons/ionicons";
 
 @Component({
   selector: 'app-position',
   templateUrl: './position.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    ProgressSpinnerComponent
+    ProgressSpinnerComponent,
+    NgIcon
   ],
-  standalone: true
+  viewProviders: [
+    provideIcons({
+      ionRefresh
+    }),
+  ]
 })
 export class PositionComponent {
   protected readonly locationService = inject(LocationService);
