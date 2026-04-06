a33c4b3c5cffa7a5be46b48ba9223186c4adf09a
Alexander Strutz <alex-strutz@t-online.de>
2026-04-06 20:09:28 +0200

    refactor: Rename amazing-bordeaux to amazing

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
     },
     extend: {
       colors: {
-        'amazing-bordeaux': '#5F2234',
-        'amazing-bordeaux-dark': '#210d11',
-        'amazing-bordeaux-light': '#aa6b7e',
-        'amazing-bordeaux-accent': '#f43f5e',
-        'amazing-bordeaux-text-primary': '#ffe4e6',
-        'amazing-bordeaux-text-secondary': '#ffe4e680',
+        'amazing': '#5F2234',
+        'amazing-dark': '#210d11',
+        'amazing-light': '#aa6b7e',
+        'amazing-accent': '#f43f5e',
+        'amazing-text-primary': '#ffe4e6',
+        'amazing-text-secondary': '#ffe4e680',
       },
       boxShadow: {
         inputs: '0 0 0 .2rem #aa6b7e',
