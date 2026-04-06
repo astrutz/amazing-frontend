beddf353653c4c66d8b336785f34773215c76b71
Alexander Strutz <alex-strutz@t-online.de>
2026-04-06 18:14:01 +0200

    feat: Include new icon buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CreateComponent } from './components/create/create.component';
 import { SearchComponent } from './components/search/search.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +13,14 @@ export const routes: Routes = [
   {
     path: 'search',
     component: SearchComponent,
+  },
+  {
+    path: 'stats',
+    component: StatsComponent,
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
   },
   {
     path: '',
