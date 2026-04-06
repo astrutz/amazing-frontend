b5588b3abf56280a001d024ee6fe9ea70d824e4a
Alexander Strutz <alex-strutz@t-online.de>
2026-04-06 19:50:34 +0200

    feat: Include different widths for page component

--- src/app/components/shared/page/page.component.ts
+++ src/app/components/shared/page/page.component.ts
@@ -2,13 +2,14 @@ import { Component, input } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { RouterLink } from '@angular/router';
 import { lucideX } from '@ng-icons/lucide';
+import { NgClass } from '@angular/common';
 
 /**
  * Wrapper component for a subpage to contain background, closing icon and title
  */
 @Component({
   selector: 'app-page',
-  imports: [NgIcon, RouterLink],
+  imports: [NgIcon, RouterLink, NgClass],
   templateUrl: './page.component.html',
   viewProviders: [
     provideIcons({
@@ -26,4 +27,9 @@ export class PageComponent {
    * Subtitle, only gets shown when a title is present
    */
   public subTitle$ = input<string | null>(null);
+
+  /**
+   * Subtitle, only gets shown when a title is present
+   */
+  public width$ = input<'small' | 'large'>('small');
 }
